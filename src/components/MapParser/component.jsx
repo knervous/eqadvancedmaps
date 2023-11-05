1719716c9ab7007633a23a9db5a7b7f3cc255fcf
Paul Johnson <Paul@PJOHNSOMAC-6366.local>
2023-11-05 03:36:32 -0500

    version bump

--- src/components/MapParser/component.jsx
+++ src/components/MapParser/component.jsx
@@ -10,7 +10,7 @@ import { Button } from '@mui/material';
 
 const dotNetLink = 'https://dotnet.microsoft.com/en-us/download/dotnet/thank-you/runtime-desktop-5.0.17-windows-x64-installer';
 
-const version = '0.1.3';
+const version = '0.2.0';
 
 export const MapParser = () => {
   return (
